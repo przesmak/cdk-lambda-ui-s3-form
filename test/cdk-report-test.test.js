c94e4cb6081947dd43561d8b73b817da151fab12
smakulp <przemyslaw.smakulski@technipfmc.com>
2022-03-10 15:33:41 +0100

    Initial commit

---
+++ test/cdk-report-test.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CdkReportTest = require('../lib/cdk-report-test-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-report-test-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CdkReportTest.CdkReportTestStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
